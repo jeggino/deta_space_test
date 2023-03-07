f41d7f1ccedfca3aa4defd47ea89aaf6334e5219
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-07 09:43:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import mysql.connector
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
@@ -13,7 +12,6 @@ conn = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
