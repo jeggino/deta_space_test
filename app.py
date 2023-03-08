68e28a845367a2fafbfdb4819d68fd9cc555a847
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-08 07:57:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ import pandas as pd
 import mysql.connector
 import toml
 
-# Reading data
-toml_data = toml.load(".streamlit/secrets.toml")
-# saving each credential into a variable
-HOST_NAME = toml_data['mysql']['host']
-DATABASE = toml_data['mysql']['database']
-PASSWORD = toml_data['mysql']['password']
-USER = toml_data['mysql']['user']
-PORT = toml_data['mysql']['port']
+# # Reading data
+# toml_data = toml.load(".streamlit/secrets.toml")
+# # saving each credential into a variable
+# HOST_NAME = toml_data['mysql']['host']
+# DATABASE = toml_data['mysql']['database']
+# PASSWORD = toml_data['mysql']['password']
+# USER = toml_data['mysql']['user']
+# PORT = toml_data['mysql']['port']
 
-st.write(PASSWORD)
+# st.write(PASSWORD)
 # # Using the variables we read from secrets.toml
 # mydb = mysql.connector.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
 
 # query = pd.read_sql('SELECT * FROM mytable;' , mydb)
 
 # st.dataframe(query)
+
+from sqlalchemy import create_engine
+import pandas as pd
+
+db_connection_str = 'mysql+pymysql://root:Platinum79@127.0.0.1/pets'
+db_connection = create_engine(db_connection_str)
+
+df = pd.read_sql('SELECT * FROM mytable', con=db_connection)
