5fc8aced985ce66ef6199201291f3ebe9a6a159d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-03-08 07:44:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import pandas as pd
 import tolm
-import mysql-connector-python
+import mysql.connector
 
 # Reading data
 toml_data = toml.load("secrets.toml")
@@ -15,7 +15,7 @@ USER = toml_data['mysql']['user']
 PORT = toml_data['mysql']['port']
 
 # Using the variables we read from secrets.toml
-mydb = connection.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
+mydb = mysql.connector.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
 
 query = pd.read_sql('SELECT * FROM mytable;' , mydb)
 
